0fdcf63eb5809c6a468cc6f11f20a681ac3124ae
Jack Murdock <kcajmagic@gmail.com>
2019-01-07 10:10:18 -0800

    Inital Commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+)
+
+func application(arguments []string) int {
+
+	fmt.Println("Starting Application")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(200)
+		writer.Write([]byte("Hi"))
+	})
+
+	http.ListenAndServe(":8080", router)
+
+	return 0
+}
+
+func main() {
+	os.Exit(application(os.Args))
+}
